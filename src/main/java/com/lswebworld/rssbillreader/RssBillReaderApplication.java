f691b9ac7b9d9a450d2da006e6197c26b09d2d0d
Larry Shields <larry.shields@gmail.com>
2022-06-26 19:05:57 -0400

    Git Ignore Add

---
+++ src/main/java/com/lswebworld/rssbillreader/RssBillReaderApplication.java
@@ -0,0 +1,13 @@
+package com.lswebworld.rssbillreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RssBillReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RssBillReaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lswebworld/rssbillreader/RssBillReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lswebworld.rssbillreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RssBillReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
